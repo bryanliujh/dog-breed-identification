94dc8d731e4bb22da7d8e531e92c2c76655a5d54
bryanliu <bryan.liu.jihao..dev@gmail.com>
2019-02-09 21:20:39 +0800

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications.inception_v3 import InceptionV3
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from keras.optimizers import Adam
 
@@ -9,32 +10,41 @@ import numpy as np
 from tqdm import tqdm
 import cv2
 from sklearn.model_selection import train_test_split
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
-RANDOM_SEED = 100
-top_x_class = 120
-EPOCHS_NUM = 10
+#The goal of this kaggle competition is to minimise log loss between the predicted probability and observed target
+#The submission will be a predictions of probability each dog will belong to a certain breed (e.g sample_submission.csv)
+#After submission kaggle will do the calculation of the log loss automatically
+#There are a total of 120 breeds of dogs
+
+#what parameters to choose? https://www.kaggle.com/c/dog-breed-identification/discussion/46201
+#https://www.kaggle.com/c/dog-breed-identification/discussion/43203
+
+
+#seed value initialise the randomnisation, you can set any values for the seed
+# using the same seed number guarantees the CNN will produce the same result all the time
+
+#https://stackoverflow.com/questions/45943675/meaning-of-validation-steps-in-keras-sequential-fit-generator-parameter-list
+#steps_per_epoch = TotalTrainingSamples / TrainingBatchSize
+#validation_steps = TotalvalidationSamples / ValidationBatchSize
+
+#BREED_NUM = 120
+#EPOCHS_NUM = 30
+BREED_NUM = 2
+EPOCHS_NUM = 1
+
+
+#Dimensions (height x weight) to be no smaller than 75
+DIMENSIONS = 299
+
+#https://stats.stackexchange.com/questions/153531/what-is-batch-size-in-neural-network
+BATCH_SIZE = 35
 
 df_train = pd.read_csv('labels.csv')
 df_test = pd.read_csv('sample_submission.csv')
 
-'''
-# Take a look at the class/breed distribution
-ax=pd.value_counts(df_train['breed'],ascending=True).plot(kind='barh',
-                                                       fontsize="5",
-                                                       title="Class Distribution",
-                                                       figsize=(50,100))
-ax.set(xlabel="Images per class", ylabel="Classes")
-ax.xaxis.label.set_size(40)
-ax.yaxis.label.set_size(40)
-ax.title.set_size(60)
-plt.show()
-'''
 
-# Get the top 20 breeds for now so that it is less computation
-top_breeds = sorted(list(df_train['breed'].value_counts().head(top_x_class).index))
+# You define the top X breeds by changing BREED_NUM so that it is less computation during testing, but for submission must use 120
+top_breeds = sorted(list(df_train['breed'].value_counts().head(BREED_NUM).index))
 df_train = df_train[df_train['breed'].isin(top_breeds)]
 
 one_hot = pd.get_dummies(df_train['breed'], sparse=True)
@@ -45,59 +55,58 @@ df_train['image_path'] = df_train.apply( lambda x: ('train/' + x["id"] + ".jpg" 
 print(df_train.head())
 
 # Convert the images to arrays which is used for the model. Inception uses image sizes of 299 x 299
-train_data = np.array([img_to_array(load_img(img, target_size=(299, 299))) for img in tqdm(df_train['image_path'].values.tolist())]).astype('float32')
+train_data = np.array([img_to_array(load_img(img, target_size=(DIMENSIONS, DIMENSIONS))) for img in tqdm(df_train['image_path'].values.tolist())]).astype('float32')
 
 # Split the data into train and test. The stratify parm will ensure train and test will have the same proportions of class labels as the input dataset
-x_train, x_test, y_train, y_test = train_test_split(train_data, df_train['breed'], test_size=0.2, stratify=np.array(df_train['breed']), random_state=RANDOM_SEED)
+x_train, x_validation, y_train, y_validation = train_test_split(train_data, df_train['breed'], test_size=0.2, stratify=np.array(df_train['breed']), random_state=100)
 
 print ('x_train shape = ', x_train.shape)
-print ('x_test shape = ', x_test.shape)
+print ('x_validation shape = ', x_validation.shape)
 
-'''
-# Calculate the value counts for train and test data and plot to show a good stratify, the plot should show an equal percentage split for each class
-data = y_train.value_counts().sort_index().to_frame()   # this creates the data frame with train numbers
-data.columns = ['train']   # give the column a name
-data['test'] = y_test.value_counts().sort_index().to_frame()   # add the test numbers
-new_plot = data[['train','test']].sort_values(['train']+['test'], ascending=False)   # sort the data
-new_plot.plot(kind='bar', stacked=True)
-plt.show()
+train_sample_size, n, p, q = x_train.shape
+validation_sample_size, n, p, q = x_validation.shape
 
-'''
+VALIDATION_STEPS = int(validation_sample_size / BATCH_SIZE)
+STEPS_PER_EPOCH = int(train_sample_size / BATCH_SIZE)
+
 
 # Need to convert the prediction train and test labels into one hot encoded format
 y_train = pd.get_dummies(y_train.reset_index(drop=True)).as_matrix()
-y_test = pd.get_dummies(y_test.reset_index(drop=True)).as_matrix()
+y_validation = pd.get_dummies(y_validation.reset_index(drop=True)).as_matrix()
 
 
 
-
+#image generator is used to handle the image manipulation eg. shearing, flipping
+# https://medium.com/@arindambaidya168/https-medium-com-arindambaidya168-using-keras-imagedatagenerator-b94a87cdefad
 # Create train generator.
+#set shuffle to False, because you need to yield the images in “order”, to predict the outputs and match them with their unique ids or filenames.
 train_datagen = ImageDataGenerator(rescale=1./255,
                                    rotation_range=30,
-                                   # zoom_range = 0.3,
                                    width_shift_range=0.2,
                                    height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
                                    horizontal_flip = 'true')
-train_generator = train_datagen.flow(x_train, y_train, shuffle=False, batch_size=10, seed=10)
+train_generator = train_datagen.flow(x_train, y_train, shuffle=False, batch_size=BATCH_SIZE, seed=10)
 
 
-# Create test generator
-test_datagen = ImageDataGenerator(rescale = 1./255)
-test_generator = train_datagen.flow(x_test, y_test, shuffle=False, batch_size=10, seed=10)
+# Create validation generator
+validation_datagen = ImageDataGenerator(rescale = 1./255)
+validation_generator = train_datagen.flow(x_validation, y_validation, shuffle=False, batch_size=BATCH_SIZE, seed=10)
 
-
+#https://towardsdatascience.com/keras-transfer-learning-for-beginners-6c9b8b7143e
 # Get the InceptionV3 model so we can do transfer learning
-base_model = InceptionV3(weights = 'imagenet', include_top = False, input_shape=(299, 299, 3))
-
+base_model = InceptionV3(weights = 'imagenet', include_top = False, input_shape=(DIMENSIONS, DIMENSIONS, 3))
+#base_model = InceptionResNetV2(weights = 'imagenet', include_top = False, input_shape=(DIMENSIONS, DIMENSIONS, 3))
 
 # Add a global spatial average pooling layer
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 
-# Add a fully-connected layer and a logistic layer with 20 classes
-#(there will be 120 classes for the final submission)
+# Add a fully-connected layer and a logistic layer with X breeds (classes)
+#(there will  120 breeds (classes) for the final submission)
 x = Dense(512, activation='relu')(x)
-predictions = Dense(top_x_class, activation='softmax')(x)
+predictions = Dense(BREED_NUM, activation='softmax')(x)
 
 # The model we will train
 model = Model(inputs = base_model.input, outputs = predictions)
@@ -106,14 +115,15 @@ model = Model(inputs = base_model.input, outputs = predictions)
 for layer in base_model.layers:
     layer.trainable = False
 
-# Compile with Adam
+#possible to experiment with SGD?
+# Compile with Adam optimisation, learning rate = 0.0001
 model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
 model.fit_generator(train_generator,
-                      steps_per_epoch = 175,
-                      validation_data = test_generator,
-                      validation_steps = 44,
+                      steps_per_epoch = STEPS_PER_EPOCH,
+                      validation_data = validation_generator,
+                      validation_steps = VALIDATION_STEPS,
                       epochs = EPOCHS_NUM,
                       verbose = 2)
 
@@ -123,27 +133,24 @@ model.fit_generator(train_generator,
 x_test = []
 for i in tqdm(df_test['id'].values):
     img = cv2.imread('test/{}.jpg'.format(i))
-    x_test.append(cv2.resize(img, (299, 299)))
+    x_test.append(cv2.resize(img, (DIMENSIONS, DIMENSIONS)))
 
 # Make it an array
 x_test = np.array(x_test, np.float32) / 255.
 
-print('xtest array')
+print('Please wait for predictions to be done..........')
 
 # Predict x_test
 predictions = model.predict(x_test, verbose=2)
 
-print('prediction')
 
 # Set column names to those generated by the one-hot encoding earlier
 col_names = one_hot.columns.values
 
-print('onehotdone')
 
 # Create the submission data.
 submission_results = pd.DataFrame(predictions, columns = col_names)
 
-print('creating submission')
 
 # Add the id as the first column
 submission_results.insert(0, 'id', df_test['id'])
@@ -152,56 +159,4 @@ submission_results.insert(0, 'id', df_test['id'])
 submission_results.to_csv('submission.csv', index=False)
 
 
-'''
-targets_series = pd.Series(df_train['breed'])
-one_hot = pd.get_dummies(targets_series, sparse = True)
-one_hot_labels = np.asarray(one_hot)
-im_size = 90
-x_train = []
-y_train = []
-x_test = []
-i = 0
-for f, breed in tqdm(df_train.values):
-    img = cv2.imread('train/{}.jpg'.format(f))
-    label = one_hot_labels[i]
-    x_train.append(cv2.resize(img, (im_size, im_size)))
-    y_train.append(label)
-    i += 1
-for f in tqdm(df_test['id'].values):
-    img = cv2.imread('test/{}.jpg'.format(f))
-    x_test.append(cv2.resize(img, (im_size, im_size)))
-y_train_raw = np.array(y_train, np.uint8)
-x_train_raw = np.array(x_train, np.float32) / 255.
-x_test  = np.array(x_test, np.float32) / 255.
-print(x_train_raw.shape)
-print(y_train_raw.shape)
-print(x_test.shape)
-num_class = y_train_raw.shape[1]
-X_train, X_valid, Y_train, Y_valid = train_test_split(x_train_raw, y_train_raw, test_size=0.3, random_state=1)
-# Create the base pre-trained model
-base_model = VGG19(#weights='imagenet',
-    weights = 'imagenet', include_top=False, input_shape=(im_size, im_size, 3))
-# Add a new top layer
-x = base_model.output
-x = Flatten()(x)
-predictions = Dense(num_class, activation='softmax')(x)
-# This is the model we will train
-model = Model(inputs=base_model.input, outputs=predictions)
-# First: train only the top layers (which were randomly initialized)
-for layer in base_model.layers:
-    layer.trainable = False
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-callbacks_list = [keras.callbacks.EarlyStopping(monitor='val_acc', patience=3, verbose=1)]
-model.summary()
-model.fit(X_train, Y_train, epochs=1, validation_data=(X_valid, Y_valid), verbose=1)
-preds = model.predict(x_test, verbose=1)
-sub = pd.DataFrame(preds)
-# Set column names to those generated by the one-hot encoding earlier
-col_names = one_hot.columns.values
-sub.columns = col_names
-# Insert the column id from the sample_submission at the start of the data frame
-sub.insert(0, 'id', df_test['id'])
-sub.head(5)
-'''
+print('Submission Created')
